5afa3af757176b2de5f6cf6fdea99bf04e601792
just-words <ethan.we@aol.com>
2014-07-31 14:57:27 -0400

    Github Build Trigger

--- just-words.js
+++ just-words.js
@@ -1,4 +1,4 @@
-/*Copyright 2014 just-words. This Source Code Form is subject to the terms of the Mozilla Public License, v.2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/*/
+/*(DELETE THESE FOUR WORDS) Copyright 2014 just-words. This Source Code Form is subject to the terms of the Mozilla Public License, v.2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/*/
 var stories = [];
 function getUserInfo() {
  alert("Not Implemented! (Sorry)");
